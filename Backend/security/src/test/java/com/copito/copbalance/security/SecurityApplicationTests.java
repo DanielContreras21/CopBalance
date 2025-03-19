719d0e07b59b681987b70839c335d1856e7b8ae1
Daniel Contreras <danielcp62.dc@gmail.com>
2025-03-19 12:40:25 -0500

    Creación del Proyecto CopBalance

---
+++ Backend/security/src/test/java/com/copito/copbalance/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copito.copbalance.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
